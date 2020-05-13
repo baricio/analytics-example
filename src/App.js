3ad7c70495b0efd27d0e25187d3e252a13c471b8
Fabricio Cunha <baricio@gmail.com>
2020-05-13 14:46:18 +0100

    add google tag manager

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import logo from './logo.svg';
 import './App.css';
 import {Link} from 'react-router-dom'
+
+const trackingId = "UA-115320538-3"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.pageview('/');
 
 function App() {
   return (
@@ -11,7 +16,7 @@ function App() {
         <p>
           Hello! lets try some events.
         </p>
-        <Link to="/about" onclick="ga('send', 'event', 'link', 'click', 'about page');" >Know about this</Link>
+        <Link to="/about" onClick={ga('send', 'event', 'link', 'click', 'about page')} >Know about this</Link>
       </header>
     </div>
   );
